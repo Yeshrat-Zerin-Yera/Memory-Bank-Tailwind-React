8ee1c00ebf4703050649f4ab0b1c175f19b1209d
unknown <yeshratzerinyera@gmail.com>
2022-11-22 00:11:44 +0600

    Statistic Added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer
+} from "recharts";
 
 const Statistics = () => {
+    const topicsInfo = useLoaderData();
+
     return (
-        <div>
-            <h1>Statistics</h1>
-        </div>
+        <section className='h-[40vh] md:h-[60vh] md:w-[60%] w-[100%] m-auto'>
+            <h1 className='my-16 text-4xl font-semibold text-[#172652]'>Statistics of Topics</h1>
+            <ResponsiveContainer width='100%'>
+                <BarChart
+                    data={topicsInfo.data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="total" fill="#8884d8" />
+                </BarChart>
+            </ResponsiveContainer>
+        </section>
     );
 };
 
