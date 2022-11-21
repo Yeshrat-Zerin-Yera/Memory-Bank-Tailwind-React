14f3893943b2b2f655b1f9158b4cee1f6eaaf8d0
unknown <yeshratzerinyera@gmail.com>
2022-11-22 03:42:26 +0600

    Eye Button Added

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const Question = ({ question }) => {
+    const [clicked, setClicked] = useState(false);
     const { options, correctAnswer } = question;
 
     const calculation = () => {
@@ -20,12 +23,20 @@ const Question = ({ question }) => {
     };
 
     return (
-        <div className='shadow-xl rounded mb-6 p-12 md:w-[80%] lg:w-[60%] m-auto text-left'>
+        <div className='shadow-xl rounded mb-6 p-12 md:w-[80%] lg:w-[60%] m-auto text-left relative'>
             <h1 className='text-2xl mb-6'>Quiz: {question.question}</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
                 {
                     options.map(option => <p key={option} className='bg-purple-100 hover:bg-purple-200 rounded p-6' onClick={calculation}>{option}</p>)
                 }
+            </div>
+            <div onClick={() => setClicked(!clicked)} className='absolute top-4 right-4 text-2xl hover:text-[#3050ad]'>
+                {
+                    clicked ? <FontAwesomeIcon icon={faEyeSlash} /> : <FontAwesomeIcon icon={faEye} />
+                }
+            </div>
+            <div className='h-6 mt-3'>
+                <p className={`${clicked ? 'block' : 'hidden'} text-green-600`}>{correctAnswer}</p>
             </div>
             <ToastContainer />
         </div>
