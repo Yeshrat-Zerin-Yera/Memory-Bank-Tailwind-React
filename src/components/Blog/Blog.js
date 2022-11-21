c19005a492aac6bc4559f45a2895a3982ce233ac
unknown <yeshratzerinyera@gmail.com>
2022-11-22 01:55:09 +0600

    Blog Updated

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -9,7 +9,7 @@ const Blog = () => {
             <h1 className='text-4xl font-extrabold mb-10'>Blog</h1>
             <p className='text-2xl text-[#0072ce] font-extrabold'>Total Blogs: {blogs.length}</p>
             {
-                blogs.map(blog => <div className='text-left p-6 m-6 rounded shadow-xl'>
+                blogs.map(blog => <div key={blog.id} className='text-left p-6 m-6 rounded shadow-xl'>
                     <h2 className='text-2xl mb-3'>{blog.title}</h2>
                     <p>{blog.body}</p>
                 </div>)
