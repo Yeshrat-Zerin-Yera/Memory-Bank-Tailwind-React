3e56b720049c19bd0ac905c96e05db0bb2ea15d5
unknown <yeshratzerinyera@gmail.com>
2022-11-22 09:14:54 +0600

    Safety Commit

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,20 +6,24 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const Question = ({ question }) => {
     const [clicked, setClicked] = useState(false);
-    const { options, correctAnswer } = question;
-
-    const calculation = () => {
+    const [selectedItem, setSelectedItem] = useState();
+    useEffect(() => {
         document.addEventListener('click', function (event) {
             event.stopPropagation();
-            const selectedItem = event.target.innerText;
-            if (selectedItem.replace(/\s/g, "") === correctAnswer.replace(/\s/g, "")) {
-                toast.success("Correct Answer !");
-            }
-            else if (selectedItem.replace(/\s/g, "") !== correctAnswer.replace(/\s/g, "")) {
-                toast.error("Wrong Answer !");
-            }
-            console.log(selectedItem.replace(/\s/g, ""), correctAnswer.replace(/\s/g, ""));
+            const newSelectedItem = event.target.innerText;
+            setSelectedItem(newSelectedItem);
         });
+    }, [selectedItem]);
+    const { options, correctAnswer } = question;
+
+    const calculation = (question) => {
+        if (selectedItem.replace(/\s/g, "") === question.correctAnswer.replace(/\s/g, "")) {
+            toast.success("Correct Answer !");
+        }
+        else if (selectedItem.replace(/\s/g, "") !== question.correctAnswer.replace(/\s/g, "")) {
+            toast.error("Wrong Answer !");
+        }
+        console.log(selectedItem.replace(/\s/g, ""), question.correctAnswer.replace(/\s/g, ""));
     };
 
     return (
@@ -27,7 +31,7 @@ const Question = ({ question }) => {
             <h1 className='text-2xl mb-6'>Quiz: {question.question}</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
                 {
-                    options.map(option => <p key={option} className='bg-purple-100 hover:bg-purple-200 rounded p-6' onClick={calculation}>{option}</p>)
+                    options.map(option => <p key={option} className='bg-purple-100 hover:bg-purple-200 rounded p-6' onClick={() => calculation(question)}>{option}</p>)
                 }
             </div>
             <div onClick={() => setClicked(!clicked)} className='absolute top-4 right-4 text-2xl hover:text-[#3050ad]'>
